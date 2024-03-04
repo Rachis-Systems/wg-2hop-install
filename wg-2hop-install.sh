773d3b2e82ef6f39877784d8023d755ab17e8fc1
Rami Sabbagh <rami.sab07@gmail.com>
2024-03-05 02:32:08 +0300

    Fix the exit option

--- wg-2hop-install.sh
+++ wg-2hop-install.sh
@@ -608,8 +608,8 @@ function manageMenu() {
 	echo "   4) Remove configuration (soft-uninstall)"
 	echo "   5) Uninstall WireGuard"
 	echo "   6) Exit"
-	until [[ ${MENU_OPTION} =~ ^[1-5]$ ]]; do
-		read -rp "Select an option [1-5]: " MENU_OPTION
+	until [[ ${MENU_OPTION} =~ ^[1-6]$ ]]; do
+		read -rp "Select an option [1-6]: " MENU_OPTION
 	done
 	case "${MENU_OPTION}" in
 	1)
